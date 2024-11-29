2c502f18708e8e7aebdfc5a4db311fd1bd40a7ce
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-11-29 10:41:00 +0700

    Create 001.selection-sort-with-c.c

---
+++ Program Struktur Data/014. Sorting (Pengurutan) pada Array Satu Dimensi CPP/Selection Sort CPP/001.selection-sort-with-c.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#define n 7
+void main()
+{ int  A[n] = {15,10,7,22,17,5,12 };
+int  X, I, K;
+printf(“Sebelum di sort\n”);
+for (I=0; I <= n-1; I++)
+      { printf(“ %d”, A[I]; )
+printf(“\n”);
+ 
+K = 0;
+while(K <= n-2)
+{ J = K;   I = K+1;
+       while(I <= n-1)
+          { if A[I] < A[J]
+            J = I;
+            I++; 
+          }
+       X = A[J];
+       A[J] = A[K]
+       A[K] = X;     
+                 
+       K++;
+}
+ 
+ printf(“Sesudah Sort \n”);
+ for(I=0; I <= n-1; I++)
+      printf(“%d”,  A[I] );
+}
